7d0a987af177c60ea014e69fa42e6e2c3989c646
Ivan Uskov <ivan.uskov@cpslabs.net>
2015-11-06 01:44:10 +0300

    Add animated initials

--- lw1/visual_stats/aboutdialog.cpp
+++ lw1/visual_stats/aboutdialog.cpp
@@ -1,22 +1,19 @@
 #include "aboutdialog.h"
 #include "ui_aboutdialog.h"
-#include <QPainter>
 #include <QtMath>
-#include <QBrush>
-#include <QColor>
-#include <QPen>
-#include <QRectF>
-#include <QDebug>
 #include <QPaintEvent>
+#include <algorithm>
 
 namespace
 {
-    static const int UPDATE_INTERVAL = 20;
-    static const int LETTER_WIDTH = 100;
-    static const int LETTER_HEIGHT = 200 ;
-    static const int ANIMATION_VICINITY = 10;
+    static const int UPDATE_INTERVAL = 10;
+    static const int LETTER_WIDTH = 90;
+    static const int LETTER_HEIGHT = 120;
+    static const int ANIMATION_VICINITY = 5;
     static const int PADDING = 20;
     static const int LETTER_MARGIN = 20;
+    static const float TIME_COEF = 0.1f;
+    static const float SIN_COEF = 0.1f;
 }
 
 AboutDialog::AboutDialog(QWidget *parent)
@@ -24,6 +21,7 @@ AboutDialog::AboutDialog(QWidget *parent)
     , m_ui(new Ui::AboutDialog)
 {
     m_ui->setupUi(this);
+    initInitialsVector();
 
     m_time.start();
     connect(&m_timer, SIGNAL(timeout()), this, SLOT(update()));
@@ -40,31 +38,98 @@ AboutDialog::~AboutDialog()
 void AboutDialog::paintEvent(QPaintEvent *event)
 {
     QDialog::paintEvent(event);
-    //m_time.restart();
-    auto x = PADDING + ;
-    auto y = sin(x) * 0.01f * m_time.elapsed();
+    float timeY = TIME_COEF * m_time.elapsed();
 
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
 
-    drawFirstNameLetter(painter, x, y);
-    x += LETTER_MARGIN + LETTER_WIDTH;
-    drawLastNameLetter(painter, x, y);
-    x += LETTER_MARGIN + LETTER_WIDTH;
-    drawPatronymicLetter(painter, x, y);
+    painter.setPen(Qt::red);
+    drawInitials(m_initialsFirstName, painter, timeY);
+    painter.setPen(Qt::green);
+    drawInitials(m_initialsLastName, painter, timeY);
+    painter.setPen(Qt::blue);
+    drawInitials(m_initialsPatronimic, painter, timeY);
 }
 
-void AboutDialog::drawFirstNameLetter(QPainter & painter, int x, int y)
+void AboutDialog::drawInitials(std::vector<QLine> const& initials, QPainter & painter, float timeY)
 {
-    painter.drawRect(QRect(x, y, LETTER_WIDTH, LETTER_HEIGHT));
+    std::for_each(initials.begin(), initials.end(), [&painter, timeY](QLine const& line){
+        auto y = sin(SIN_COEF * (line.x1() + timeY)) * ANIMATION_VICINITY + PADDING + line.y1();
+        painter.drawLine(line.x1(), y, line.x2(), y + line.y2());
+    });
 }
 
-void AboutDialog::drawLastNameLetter(QPainter & painter, int x, int y)
+void AboutDialog::initInitialsVector()
 {
-    painter.drawRect(QRect(x, y, LETTER_WIDTH, LETTER_HEIGHT));
+    int x = PADDING;
+    initFirstNameLetter(x);
+    x += LETTER_WIDTH + LETTER_MARGIN;
+    initLastNameLetter(x);
+    x += LETTER_WIDTH + LETTER_MARGIN;
+    initPatronymicLetter(x);
 }
 
-void AboutDialog::drawPatronymicLetter(QPainter & painter, int x, int y)
+void AboutDialog::initFirstNameLetter(int x)
 {
-    painter.drawRect(QRect(x, y, LETTER_WIDTH, LETTER_HEIGHT));
+    const int LETTER_PART_WIDTH = LETTER_WIDTH / 3;
+    for (int i = 0; i < LETTER_PART_WIDTH; ++i)
+    {
+        m_initialsFirstName.push_back(QLine(x + i, 0, x + i, LETTER_HEIGHT));
+    }
+
+    x += LETTER_PART_WIDTH;
+    for (int i = 0; i < LETTER_PART_WIDTH; ++i)
+    {
+        m_initialsFirstName.push_back(QLine(x + i, 0, x + i, LETTER_HEIGHT / 4));
+    }
+
+    x += LETTER_PART_WIDTH;
+    for (int i = 0; i < LETTER_PART_WIDTH; ++i)
+    {
+        m_initialsFirstName.push_back(QLine(x + i, 0, x + i, LETTER_HEIGHT));
+    }
+}
+
+void AboutDialog::initLastNameLetter(int x)
+{
+    const int LETTER_PART_WIDTH = LETTER_WIDTH / 3;
+    const int LETTER_PART_HEIGHT = LETTER_HEIGHT / 4;
+    for (int i = 0; i < LETTER_PART_WIDTH; ++i)
+    {
+        m_initialsLastName.push_back(QLine(x + i, 0, x + i, LETTER_HEIGHT));
+    }
+
+    x += LETTER_WIDTH / 3;
+    for (int i = 0, y = LETTER_HEIGHT - LETTER_PART_HEIGHT * 2; i < LETTER_PART_WIDTH; ++i, --y)
+    {
+        m_initialsLastName.push_back(QLine(x + i, y, x + i, LETTER_PART_HEIGHT * 1.5));
+    }
+
+    x += LETTER_WIDTH / 3;
+    for (int i = 0; i < LETTER_PART_WIDTH; ++i)
+    {
+        m_initialsLastName.push_back(QLine(x + i, 0, x + i, LETTER_HEIGHT));
+    }
+}
+
+void AboutDialog::initPatronymicLetter(int x)
+{
+    const int LETTER_PART_WIDTH = LETTER_WIDTH / 3;
+    const int LETTER_PART_HEIGHT = LETTER_HEIGHT / 4;
+    for (int i = 0; i < LETTER_PART_WIDTH; ++i)
+    {
+        m_initialsPatronimic.push_back(QLine(x + i, 0, x + i, LETTER_HEIGHT / 2));
+    }
+
+    x += LETTER_WIDTH / 3;
+    for (int i = 0, y = LETTER_PART_HEIGHT; i < LETTER_PART_WIDTH; ++i)
+    {
+        m_initialsPatronimic.push_back(QLine(x + i, y, x + i, LETTER_PART_HEIGHT));
+    }
+
+    x += LETTER_WIDTH / 3;
+    for (int i = 0; i < LETTER_PART_WIDTH; ++i)
+    {
+        m_initialsPatronimic.push_back(QLine(x + i, 0, x + i, LETTER_HEIGHT));
+    }
 }
--- lw1/visual_stats/aboutdialog.h
+++ lw1/visual_stats/aboutdialog.h
@@ -3,6 +3,9 @@
 #include <QDialog>
 #include <QTime>
 #include <QTimer>
+#include <QLine>
+#include <QPainter>
+#include <vector>
 
 namespace Ui {
 class AboutDialog;
@@ -24,8 +27,14 @@ private:
     Ui::AboutDialog *m_ui;
     QTime m_time;
     QTimer m_timer;
+    std::vector<QLine> m_initialsFirstName;
+    std::vector<QLine> m_initialsLastName;
+    std::vector<QLine> m_initialsPatronimic;
 
-    void drawFirstNameLetter(QPainter & painter, int x, int y);
-    void drawLastNameLetter(QPainter & painter, int x, int y);
-    void drawPatronymicLetter(QPainter & painter, int x, int y);
+    void drawInitials(std::vector<QLine> const& initials, QPainter & painter, float timeY);
+
+    void initInitialsVector();
+    void initFirstNameLetter(int x);
+    void initLastNameLetter(int x);
+    void initPatronymicLetter(int x);
 };
